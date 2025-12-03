8df97eb5b80902c17ceeca0e39832fa22f2fde01
kanehiro <kanehiro@gmail.com>
2025-12-03 10:51:38 +0900

    Add files via upload

---
+++ chap06/06/app.py
@@ -0,0 +1,50 @@
+import gradio as gr
+from agents import Agent, Runner
+
+with gr.Blocks() as demo:
+    state = gr.State({
+        "last_id": None
+    })
+    # UI を定義
+    chatbot = gr.Chatbot(type="messages")
+    with gr.Row():
+        msg = gr.Textbox(show_label=False, scale=7)
+        with gr.Column(scale=1):
+            submit = gr.Button("送信")
+            clear = gr.ClearButton([msg, chatbot], value="リセット")
+
+    agent = Agent(
+        name="Assistant",
+        model="gpt-5-mini", 
+        instructions="あなたは役に立つAIアシスタントです。"
+    )
+
+    async def respond(state, message, chat_history):
+
+        """メッセージ送信時の処理"""
+        # 会話履歴にユーザーの発言を追加
+        chat_history.append({"role": "user", "content": message})
+
+        # エージェントに会話履歴を送信
+        response = await Runner.run(
+            agent, message, 
+            previous_response_id = state["last_id"],
+        )
+        bot_message = response.final_output
+        # エージェントの出力を会話履歴に追加
+        chat_history.append({"role": "assistant", "content": bot_message})
+        state["last_id"] = response.last_response_id 
+
+        # Gradio UI に会話履歴を返す
+        return "", chat_history
+
+    # Gradio UI にコールバックを設定
+    gr.on(
+        triggers=[msg.submit, submit.click],
+        fn=respond,
+        inputs=[state, msg, chatbot],
+        outputs=[msg, chatbot],
+    )
+
+if __name__ == "__main__":
+    demo.launch()
